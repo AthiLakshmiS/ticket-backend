580e99653215ce726ac1d2b9ff5fe2e2abf544a1
Athilakshmi <athilak1999@gmail.com>
2024-01-20 12:41:43 +0530

    Check for the vercel fix

--- back-end/index.js
+++ back-end/index.js
@@ -1,9 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const userRoutes = require("./routes/user");
+const ticketRoutes = require("./routes/ticket");
+const app = express();
+app.use(cors());
+require("dotenv").config();
+app.use(bodyParser.json());
+app.use(express.json()); 
+
+// Connect to the database
+const PORT = process.env.PORT || 3000;
+mongoose.connect("mongodb+srv://athilakshmi:8Ia2Tk4wZLqPSvgI@cluster0.bcy8b9g.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 // index.js
-const express = require('express')
-
-const app = express()
-const PORT = 4000
-
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
@@ -18,3 +33,13 @@ app.get('/about', (req, res) => {
 
 // Export the Express API
 module.exports = app
+// //Routers
+// app.use('/api/support-agents', userRoutes);
+// app.use('/api/assigned-users', userRoutes);
+// app.use('/api/support-tickets', ticketRoutes);
+
+// // Start the server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+// module.exports = app;
