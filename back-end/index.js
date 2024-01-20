735b19c1011eb499478d2cfa4df2b05e7c06d056
Athilakshmi <athilak1999@gmail.com>
2024-01-20 11:53:35 +0530

    updated modules

--- back-end/index.js
+++ back-end/index.js
@@ -5,26 +5,24 @@ const bodyParser = require('body-parser');
 const userRoutes = require("./routes/user");
 const ticketRoutes = require("./routes/ticket");
 const app = express();
-app.use(cors())
+app.use(cors());
 require("dotenv").config();
 app.use(bodyParser.json());
 app.use(express.json()); 
 
 // Connect to the database
 const PORT = process.env.PORT || 3000;
-mongoose.connect("mongodb+srv://athilakshmi:8Ia2Tk4wZLqPSvgI@cluster0.bcy8b9g.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
-// db.dropDatabase();
 
 //Routers
 app.use('/api/support-agents', userRoutes);
 app.use('/api/assigned-users', userRoutes);
 app.use('/api/support-tickets', ticketRoutes);
-app.use('/api/get-tasks', ticketRoutes);
 
 // Start the server
 app.listen(PORT, () => {
