813b0433f7832a57bbd17bb4ca7a738fc2094119
Athilakshmi <athilak1999@gmail.com>
2024-01-20 13:07:30 +0530

    Update the api links

--- back-end/index.js
+++ back-end/index.js
@@ -12,12 +12,12 @@ app.use(express.json());
 
 // Connect to the database
 const PORT = process.env.PORT || 3000;
-// mongoose.connect("mongodb+srv://athilakshmi:8Ia2Tk4wZLqPSvgI@cluster0.bcy8b9g.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// db.once('open', () => {
-//   console.log('Connected to MongoDB');
-// });
+mongoose.connect("mongodb+srv://athilakshmi:8Ia2Tk4wZLqPSvgI@cluster0.bcy8b9g.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 // index.js
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
@@ -32,14 +32,14 @@ app.get('/about', (req, res) => {
 })
 
 // Export the Express API
-module.exports = app
+// module.exports = app
 // //Routers
-// app.use('/api/support-agents', userRoutes);
-// app.use('/api/assigned-users', userRoutes);
-// app.use('/api/support-tickets', ticketRoutes);
+app.use('/api/support-agents', userRoutes);
+app.use('/api/assigned-users', userRoutes);
+app.use('/api/support-tickets', ticketRoutes);
 
-// // Start the server
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
-// module.exports = app;
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+module.exports = app;
