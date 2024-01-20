791aced27795f26f89e149e063bb24db6105ba1b
Athilakshmi <athilak1999@gmail.com>
2024-01-20 19:27:05 +0530

    pushed the vercel file

--- back-end/index.js
+++ back-end/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const userRoutes = require("./routes/user");
 const ticketRoutes = require("./routes/ticket");
+const User = require("./models/userModel");
+
 const app = express();
 app.use(cors());
 require("dotenv").config();
@@ -22,11 +24,38 @@ db.once('open', () => {
 // Export the Express API
 // module.exports = app
 // //Routers
-app.use('/api/support-agents', userRoutes);
-app.use('/api/assigned-users', userRoutes);
-app.use('/api/support-tickets', ticketRoutes);
+// app.use('/api/support-agents', userRoutes);
+// app.use('/api/assigned-users', userRoutes);
+// app.use('/api/support-tickets', ticketRoutes);
+const asyncHandler = fn => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
 
-// Start the server
+app.get('/support-tickets', asyncHandler(async (req, res) => {
+  try {
+    // const firstActiveUser = await User.findOne({ active: true });
+    const firstActiveUser = await User.find({});
+    
+    if (!firstActiveUser) {
+      return res.status(404).json({
+        status: 'Failed',
+        message: 'No active users found.',
+      });
+    }
+
+    res.status(200).json({
+      status: 'Success',
+      data: {
+        user: firstActiveUser,
+      },
+    });
+  } catch (error) {
+    console.error('Error fetching first active user:', error.message);
+    res.status(500).json({
+      status: 'Failed',
+      message: 'Internal Server Error',
+    });
+  }
+}));// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
